a4b279d75c4be3efb9c6de7854ced3333b6ca11e
me_me <ichshlanger2@gmail.com>
2024-03-21 21:11:21 +0300

    Polynomial Regression model

---
+++ src/model.py
@@ -0,0 +1,36 @@
+import numpy
+
+
+class PolynomialRegression:
+    def __init__(self)->None:
+        # constructor will be consisting different parameters of model. For instance: lr, epochs, etc.
+        pass
+
+    # Do i need this?
+    def add_interception(self):
+        # add extra column of ones to our data instread further calculations both weights and bias
+        pass
+
+    def weights_initialization(self):
+        # choise of different types of weigths initialization. For instance: zeros, ones, some specific values, etc.
+        pass
+
+    def polynom_weights(self):
+        # add new polynomial features x^2, x^3 and so on.
+        pass
+
+    def train(self):
+        # train of our model
+
+
+        # space for: 
+        # adding interception, 
+        # adding polynomial features
+        # weights initializations
+
+
+        # train loop
+        pass
+
+    def predict(self):
+        pass
