932e9a327d932bf08d99ac75c52fa0f2545d7528
me_me <ichshlanger2@gmail.com>
2024-03-23 00:29:21 +0300

    preprocessing functions

---
+++ src/preprocessing.py
@@ -0,0 +1,30 @@
+import numpy
+import pandas
+import seaborn
+
+wine_quality = pandas.read_csv("./data/raw/winequality-red.csv")
+
+corr_matrix = wine_quality.corr(method='pearson')
+seaborn.heatmap(corr_matrix, annot=False)
+
+wine_quality = wine_quality.drop(columns='free sulfur dioxide')
+
+def get_dataset(name:str)->pandas.DataFrame:
+    match name:
+        case "wine_quality":
+            return wine_quality
+        
+        case _:
+            raise(NameError)
+
+def split(dataset:pandas.DataFrame):
+    # It needs to split our data into subdatasets for training, validations and testing
+    pass
+
+def normalize(dataset:numpy.ndarray)->numpy.ndarray:
+    # normalization of our data
+    pass
+
+def standardize(dataset:numpy.ndarray, mean:float, std:float)->numpy.ndarray:
+    # move our data to standard normal distribution
+    pass
